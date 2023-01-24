c92eac6d14e1fc7e306dd6eb2cfd6ddb7317a1cb
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-01-23 23:33:59 -0300

    Mais um exercicio feito

--- _4_classesAtributosMetodosMembrosEstaticos/_8_Exercicio_6/Aluno.cs
+++ _4_classesAtributosMetodosMembrosEstaticos/_8_Exercicio_6/Aluno.cs
@@ -1,13 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace _8_Exercicio_6
 {
     class Aluno
     {
-        public double Nota1;
-        public double Nota2;
-        public double Nota3;
+        public string Nome;
+        private double Nota1;
+        private double Nota2;
+        private double Nota3;
+        private string Aprovacao;
+
+        public void DefinirNota(int Trimestre, double Nota)
+        {
+            if (Trimestre == 1)
+            {
+                if(Nota <= 30)
+                {
+                    Nota1 = Nota;
+                } 
+                else
+                {
+                    Console.WriteLine("Nota nao pode ser maior que 30.");
+                    Console.Write("Tente novamente: ");
+                    DefinirNota(1, double.Parse(Console.ReadLine()));
+                }
+            } 
+            else if (Trimestre == 2 || Trimestre == 3)
+            {
+                if (Nota <= 35)
+                {
+                    if (Trimestre == 2)
+                    {
+                        Nota2 = Nota;
+                    } 
+                    else
+                    {
+                        Nota3 = Nota;
+                        Resultado(Nota1, Nota2, Nota3);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nota nao pode ser maior que 35.");
+                    Console.Write("Tente novamente: ");
+                    if (Trimestre == 2)
+                    {
+                        DefinirNota(2, int.Parse(Console.ReadLine()));
+                    }
+                    else
+                    {
+                        DefinirNota(3, int.Parse(Console.ReadLine()));
+                    }
+                    
+                }
+            }
+        }
+
+        private void Resultado(double n1, double n2, double n3)
+        {
+            double Final = n1 + n2 + n3;
+            if(Final >= 60)
+            {
+                Aprovacao = "Aprovado";
+            } else
+            {
+                Aprovacao = "Reprovado";
+                Console.WriteLine($"Faltaram: {(Final - 60) * -1:F2} pontos");
+            }
+            Console.WriteLine($"Nota final = {Final:F2}");
+            Console.WriteLine($"{Aprovacao}");
+        }
     }
 }
--- _4_classesAtributosMetodosMembrosEstaticos/_8_Exercicio_6/Program.cs
+++ _4_classesAtributosMetodosMembrosEstaticos/_8_Exercicio_6/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
+//Sistema que calcula as notas dos três trimestres e dá o resultado se foi aprovado ou reprovado. Se reprovado, ele mostrará quantos pontos faltaram para ser aprovado.
+
 namespace _8_Exercicio_6
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Aluno f;
+            f = new Aluno();
+
+            Console.Write("Digite o nome do aluno: ");
+            f.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno");
+            Console.Write("Primeiro trimestre: ");
+            f.DefinirNota(1, double.Parse(Console.ReadLine()));
+            Console.Write("Segundo trimestre: ");
+            f.DefinirNota(2, double.Parse(Console.ReadLine()));
+            Console.Write("Terceiro trimestre: ");
+            f.DefinirNota(3, double.Parse(Console.ReadLine()));
         }
     }
 }
