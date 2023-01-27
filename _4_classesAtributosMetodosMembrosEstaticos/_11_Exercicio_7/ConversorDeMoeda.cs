7a2347d7e8193fbe564c705f9553336d45bb38f0
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-01-26 22:44:53 -0300

    Exercicio de fixação de membros estáticos

---
+++ _4_classesAtributosMetodosMembrosEstaticos/_11_Exercicio_7/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11_Exercicio_7
+{
+    class ConversorDeMoeda
+    {
+        public static double iof = 0.06;
+
+        public static double ComprarDolares(double cotacao, double dolares)
+        {
+            double reais = dolares * cotacao;
+            return reais + (reais * iof);
+        }
+    }
+}
---
+++ _4_classesAtributosMetodosMembrosEstaticos/_11_Exercicio_7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//Exercício de câmbio monetário
+
+namespace _11_Exercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolares = double.Parse(Console.ReadLine());
+
+            double reais = ConversorDeMoeda.ComprarDolares(cotacao, dolares);
+            Console.WriteLine($"Valor a ser pago em reais = {reais:F2}");
+        }
+    }
+}
