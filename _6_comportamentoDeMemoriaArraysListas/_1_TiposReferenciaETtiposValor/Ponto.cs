6cc201dfe5af71bca8d57c107c40a75df3795970
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-01-27 21:28:23 -0300

    Struct e class

---
+++ _6_comportamentoDeMemoriaArraysListas/_1_TiposReferenciaETtiposValor/Ponto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_TiposReferenciaETtiposValor
+{
+    struct Ponto //Usando struct ao invés de apontar um endereço de memória, eles mesmos armazenarão os valores.
+    {
+        public int x;
+        public int y;
+
+        public override string ToString()
+        {
+            return $"({x},{y})";
+        }
+    }
+
+    //Class as variáveis são armazenadas por ponteiros de memória, já struct é com caixas.
+}
---
+++ _6_comportamentoDeMemoriaArraysListas/_1_TiposReferenciaETtiposValor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1_TiposReferenciaETtiposValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ponto p; //new Ponto() > por estar usando struct, esse código não é mais necessário pra ser inicializada
+
+            p.x = 10;
+            p.y = 12;
+            Console.WriteLine(p);
+
+            p = new Ponto();
+            Console.WriteLine(p);
+        }
+    }
+}
