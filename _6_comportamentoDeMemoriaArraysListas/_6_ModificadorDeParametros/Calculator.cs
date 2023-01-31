a725ac15b3501e8d676dd4143be2d73a43c0ef5f
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-01-30 23:44:19 -0300

    Modificador de parametros

---
+++ _6_comportamentoDeMemoriaArraysListas/_6_ModificadorDeParametros/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_ModificadorDeParametros
+{
+    class Calculator
+    {
+        //Ao invés de criar vários construtores com quantidade de parametros
+        //diferentes para as várias possibilidades, podemos dessa forma
+        //passar quantos parametros quisermos
+        public static int Sum(params int[] numbers)
+        {
+
+            int sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ _6_comportamentoDeMemoriaArraysListas/_6_ModificadorDeParametros/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _6_ModificadorDeParametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calculator.Sum(1, 2, 3, 4);
+            Console.WriteLine(result);
+        }
+    }
+}
