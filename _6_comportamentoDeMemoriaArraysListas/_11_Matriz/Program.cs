8068d2f02c2230c82872c05f43c9932f07fea0e7
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-02-09 23:48:41 -0300

    Exercício de matriz

---
+++ _6_comportamentoDeMemoriaArraysListas/_11_Matriz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _11_Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length);
+            Console.WriteLine(mat.Rank); //Quantidade de linhas
+            Console.WriteLine(mat.GetLength(0));
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
---
+++ _6_comportamentoDeMemoriaArraysListas/_12_Exercicio/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _12_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tamanho da matriz em linhas e colunas: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+
+            for(int i = 0; i < matriz.GetLength(0); i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for(int j = 0; j < matriz.GetLength(0); j++)
+                {
+                    matriz[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonal principal:");
+            for(int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.Write($"{matriz[i,i]} ");
+            }
+            Console.WriteLine();
+            int count = 0;
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(0); j++)
+                {
+                    if(matriz[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Números negativos: {count}");
+
+        }
+    }
+}
