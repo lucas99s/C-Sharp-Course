2fa5da7cb8bc0c19e01309c783863ad1406f19e2
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-02-24 23:48:45 -0300

    Organizando as classes do exercício

---
+++ _8_enumeracoesEComposicao/_2_Exercicio/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_Exercicio.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ _8_enumeracoesEComposicao/_2_Exercicio/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace _2_Exercicio.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
