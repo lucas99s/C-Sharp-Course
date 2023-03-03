c9e79e564d37013d6cb69b4456b0ca8d8215810f
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-03-02 23:51:11 -0300

    Herança

---
+++ _9_herancaEPolimorfismo/_1_Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace _1_Heranca.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+        
+        public BusinessAccount() { }
+
+        public BusinessAccount(double loanLimit, int number, string holder, double balance) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Load(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ _9_herancaEPolimorfismo/_1_Heranca/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using _1_Heranca.Entities;
+
+namespace _1_Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(3500, 8080, "Lucas", 15000);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance = 200; Não vai funcionar pois é uma variável protected, ou seja, a própria classe e subclasses podem modificá-la externamente, mas outras classes como essa não.
+        }
+    }
+}
