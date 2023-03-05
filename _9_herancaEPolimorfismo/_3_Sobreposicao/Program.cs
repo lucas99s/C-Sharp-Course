d61246bdd3d858c5225780bb93158c1df4ab2b24
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-03-04 23:30:44 -0300

    Sobreposição, palavras virtual, override e base

---
+++ _9_herancaEPolimorfismo/_3_Sobreposicao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using _3_Sobreposicao.Entities;
+
+namespace _3_Sobreposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1000, "Lucas", 500);
+            Account acc2 = new SavingsAccount(1001, "Anna", 500, 0.01);
+
+            acc1.Withdraw(10);
+            acc2.Withdraw(10);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
