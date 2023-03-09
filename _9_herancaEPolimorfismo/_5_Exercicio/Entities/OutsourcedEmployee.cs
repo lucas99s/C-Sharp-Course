b08861cf7aabeb9a291c08ab499873f18c987dd5
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-03-08 23:52:33 -0300

    Organização das classes

---
+++ _9_herancaEPolimorfismo/_5_Exercicio/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5_Exercicio.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(double additionalCharge, string name, int hours, double valuePerHour) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return (Hours * ValuePerHour) + (AdditionalCharge + (AdditionalCharge * 0.1));
+        }
+    }
+}
