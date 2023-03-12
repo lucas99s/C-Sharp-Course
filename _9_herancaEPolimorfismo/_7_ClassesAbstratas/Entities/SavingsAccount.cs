9b0b71b95b36197358022a2affcaeb130ad212ca
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-03-11 21:49:57 -0300

    Classes abstratas

---
+++ _9_herancaEPolimorfismo/_7_ClassesAbstratas/Entities/SavingsAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7_ClassesAbstratas.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        //Saque sem desconto
+
+        //public override void Withdraw(double amount)
+        //{
+        //    Balance -= amount;
+        //}
+
+        //Saque com desconto da superclasse + desconto posterior na subclasse.
+        //Ou seja, base executará a função Withdraw da superclasse, e depois o que estiver nessa abaixo.
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2;
+        }
+    }
+}
---
+++ _9_herancaEPolimorfismo/_7_ClassesAbstratas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using _7_ClassesAbstratas.Entities;
+
+namespace _7_ClassesAbstratas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Classes abstratas são classes que não podem ser instanciadas.
+            //É uma forma de garantir herança total: somente subclasses não abstratas podem ser instanciadas, mas nunca a superclasse abstrata.
+
+            //Mesmo com a Account abstrata, ainda podemos fazer o uso dela, apenas não podemos instanciar, pois vai dar erro.
+            List<Account> account = new List<Account>();
+
+            account.Add(new SavingsAccount(1001, "Lucas", 500, 0.01));
+            account.Add(new BusinessAccount(1002, "Roberta", 500, 400));
+            account.Add(new SavingsAccount(1003, "Luciano", 500, 0.01));
+            account.Add(new BusinessAccount(1004, "Talita", 500, 400));
+
+            double sum = 0;
+            foreach(Account a in account)
+            {
+                sum += a.Balance;
+            }
+            Console.WriteLine(sum);
+
+            foreach(Account a in account)
+            {
+                a.Withdraw(10);
+            }
+
+            foreach (Account a in account)
+            {
+                Console.WriteLine($"Updated balance of the account: {a.Number}: R$ {a.Balance:F2}");
+            }
+
+        }
+    }
+}
